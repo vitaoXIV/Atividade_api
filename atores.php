69b3704b7c778d9068b2c421d4cd0808e6c688ba
vitaoXIV <ve569933@gmail.com>
2024-07-14 23:42:18 -0300

    first commit

---
+++ atores.php
@@ -0,0 +1,8 @@
+<?php 
+require_once 'cabecalho.php';
+?>
+
+<body>
+
+</body>
+</html>
---
+++ cabecalho.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>atividade</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+<a href="atores.php" >Atores</a>
+<a href="">STAR WARS</a>
+<a href="personagens.php">Personagens</a>
+
+
+    </nav>
+    
+</html>
